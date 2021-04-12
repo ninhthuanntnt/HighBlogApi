95d98f76e9d9ef084082c2b5400812826cd2b447
ninhthuanntnt <ninhthuan1999@gmail.com>
2021-04-12 15:33:43 +0700

    [BE][USER] Init paypal config

---
+++ src/main/java/com/high/highblog/bloc/payment/PaypalPaymentBloc.java
@@ -0,0 +1,9 @@
+package com.high.highblog.bloc.payment;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class PaypalPaymentBloc {
+}
