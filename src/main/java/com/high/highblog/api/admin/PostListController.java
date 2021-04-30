21793f5f216f9f64e8c395fd848fc7c2f5a6caad
ducminh <ducminhle231@gmail.com>
2021-05-01 00:12:23 +0700

    [BE][ADMIN] Create api fetch list post to filter by category id

---
+++ src/main/java/com/high/highblog/api/admin/PostListController.java
@@ -0,0 +1,35 @@
+package com.high.highblog.api.admin;
+
+import com.high.highblog.bloc.PostListBloc;
+import com.high.highblog.helper.PaginationHelper;
+import com.high.highblog.mapper.PostMapper;
+import com.high.highblog.model.dto.request.BasePaginationReq;
+import com.high.highblog.model.dto.request.PostSearchReq;
+import com.high.highblog.model.dto.response.BasePaginationRes;
+import com.high.highblog.model.entity.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("adminPostListController")
+@RequestMapping("/api/v1/admin/posts")
+public class PostListController {
+    private final PostListBloc postListBloc;
+
+    public PostListController(final PostListBloc postListBloc) {
+        this.postListBloc = postListBloc;
+    }
+
+    // TODO: bind array of String to array of Objects fot sorts field
+    @GetMapping
+    public ResponseEntity<BasePaginationRes> fetchListPost(final Long categoryId, BasePaginationReq req) {
+        Page<Post> posts = postListBloc.fetchPosts(categoryId,req);
+
+        return ResponseEntity.ok(PaginationHelper.buildBasePaginationRes(
+                posts.map(PostMapper.INSTANCE::toPostRes)
+        ));
+    }
+}
