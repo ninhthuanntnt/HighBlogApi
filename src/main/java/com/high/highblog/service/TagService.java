b14b31cbd39f981ef2e2d20a566d22c7f8b54224
ducminh <ducminhle231@gmail.com>
2021-06-27 02:00:46 +0700

    [BE][ADMIN] Create api to add new tag

--- src/main/java/com/high/highblog/service/TagService.java
+++ src/main/java/com/high/highblog/service/TagService.java
@@ -1,9 +1,13 @@
 package com.high.highblog.service;
 
+import com.high.highblog.error.exception.ValidatorException;
+import com.high.highblog.helper.SecurityHelper;
 import com.high.highblog.model.entity.Tag;
 import com.high.highblog.repository.TagRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.ObjectUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -21,5 +25,15 @@ public class TagService {
         log.info("Fetch all tags");
 
         return repository.findAll();
+    }
+    @Transactional
+    public void addNewTag(final Tag newTag) {
+        validatePostBeforeSave(newTag.getName());
+        log.info("add new tag");
+        repository.save(newTag);
+    }
+    private void validatePostBeforeSave(final String tagName){
+        if (repository.existsTagByNameEquals(tagName))
+            throw new ValidatorException("TagName already exists", "tagName");
     }
 }
